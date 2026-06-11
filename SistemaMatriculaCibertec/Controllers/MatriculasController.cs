70153d022a5f9542fd0d18ab3842f0c56dd84851
anthony azpur <anthony12al20@gmail.com>
2026-06-11 12:41:35 -0500

    layout agregador

---
+++ SistemaMatriculaCibertec/Controllers/MatriculasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SistemaMatriculaCibertec.Models;
+
+namespace SistemaMatriculaCibertec.Controllers
+{
+    public class MatriculasController : Controller
+    {
+        private SistemaMatriculaDBEntities db = new SistemaMatriculaDBEntities();
+
+        // GET: Matriculas
+        public ActionResult Index()
+        {
+            var matricula = db.Matricula.Include(m => m.Alumno).Include(m => m.Curso);
+            return View(matricula.ToList());
+        }
+
+        // GET: Matriculas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Matricula matricula = db.Matricula.Find(id);
+            if (matricula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(matricula);
+        }
+
+        // GET: Matriculas/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdAlumno = new SelectList(db.Alumno, "IdAlumno", "CodigoAlumno");
+            ViewBag.IdCurso = new SelectList(db.Curso, "IdCurso", "CodigoCurso");
+            return View();
+        }
+
+        // POST: Matriculas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdMatricula,CodigoMatricula,IdAlumno,IdCurso,FechaMatricula,Estado")] Matricula matricula)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Matricula.Add(matricula);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdAlumno = new SelectList(db.Alumno, "IdAlumno", "CodigoAlumno", matricula.IdAlumno);
+            ViewBag.IdCurso = new SelectList(db.Curso, "IdCurso", "CodigoCurso", matricula.IdCurso);
+            return View(matricula);
+        }
+
+        // GET: Matriculas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Matricula matricula = db.Matricula.Find(id);
+            if (matricula == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdAlumno = new SelectList(db.Alumno, "IdAlumno", "CodigoAlumno", matricula.IdAlumno);
+            ViewBag.IdCurso = new SelectList(db.Curso, "IdCurso", "CodigoCurso", matricula.IdCurso);
+            return View(matricula);
+        }
+
+        // POST: Matriculas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdMatricula,CodigoMatricula,IdAlumno,IdCurso,FechaMatricula,Estado")] Matricula matricula)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(matricula).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdAlumno = new SelectList(db.Alumno, "IdAlumno", "CodigoAlumno", matricula.IdAlumno);
+            ViewBag.IdCurso = new SelectList(db.Curso, "IdCurso", "CodigoCurso", matricula.IdCurso);
+            return View(matricula);
+        }
+
+        // GET: Matriculas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Matricula matricula = db.Matricula.Find(id);
+            if (matricula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(matricula);
+        }
+
+        // POST: Matriculas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Matricula matricula = db.Matricula.Find(id);
+            db.Matricula.Remove(matricula);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SistemaMatriculaCibertec/Controllers/RetiroesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SistemaMatriculaCibertec.Models;
+
+namespace SistemaMatriculaCibertec.Controllers
+{
+    public class RetiroesController : Controller
+    {
+        private SistemaMatriculaDBEntities db = new SistemaMatriculaDBEntities();
+
+        // GET: Retiroes
+        public ActionResult Index()
+        {
+            var retiro = db.Retiro.Include(r => r.Matricula);
+            return View(retiro.ToList());
+        }
+
+        // GET: Retiroes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Retiro retiro = db.Retiro.Find(id);
+            if (retiro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(retiro);
+        }
+
+        // GET: Retiroes/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdMatricula = new SelectList(db.Matricula, "IdMatricula", "CodigoMatricula");
+            return View();
+        }
+
+        // POST: Retiroes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdRetiro,IdMatricula,FechaRetiro,Motivo")] Retiro retiro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Retiro.Add(retiro);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdMatricula = new SelectList(db.Matricula, "IdMatricula", "CodigoMatricula", retiro.IdMatricula);
+            return View(retiro);
+        }
+
+        // GET: Retiroes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Retiro retiro = db.Retiro.Find(id);
+            if (retiro == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdMatricula = new SelectList(db.Matricula, "IdMatricula", "CodigoMatricula", retiro.IdMatricula);
+            return View(retiro);
+        }
+
+        // POST: Retiroes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdRetiro,IdMatricula,FechaRetiro,Motivo")] Retiro retiro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(retiro).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdMatricula = new SelectList(db.Matricula, "IdMatricula", "CodigoMatricula", retiro.IdMatricula);
+            return View(retiro);
+        }
+
+        // GET: Retiroes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Retiro retiro = db.Retiro.Find(id);
+            if (retiro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(retiro);
+        }
+
+        // POST: Retiroes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Retiro retiro = db.Retiro.Find(id);
+            db.Retiro.Remove(retiro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
